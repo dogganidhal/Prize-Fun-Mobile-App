f457a8830e0f7d347ac73bd3ea939d05eb81c400
Rex Isaac Raphael <rex.raphael@outlook.com>
2019-11-10 15:02:02 +0100

    added unityexport

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:fun_prize/model/game_engine.dart';
-import 'package:fun_prize/widgets/game/prize_fun_game.dart';
+// import 'package:fun_prize/model/game_engine.dart';
+import 'package:fun_prize/unity/demo.dart';
+// import 'package:fun_prize/widgets/game/prize_fun_game.dart';
 
 
 void main() async {
-  final engine = GameEngine();
-  final prizeAndFunGame = PrizeFunGame(engine: engine);
-  runApp(prizeAndFunGame.widget);
+  // final engine = GameEngine();
+  // final prizeAndFunGame = PrizeFunGame(engine: engine);
+
+  runApp(UnityDemo());
+  // runApp(prizeAndFunGame.widget);
 //  final user = await AuthService().currentUser();
 //  runApp(App(isAuthenticated: user != null));
 }
