42155295d8d3fdc9e579ff09deb493a3566aa1b7
DOGGA Nidhal <dogga.nidhal@gmail.com>
2019-11-13 14:07:42 +0100

    -- Add Arm64 Unity Target

--- android/app/src/main/java/io/github/dogganidhal/fun_prize/MainActivity.java
+++ android/app/src/main/java/io/github/dogganidhal/fun_prize/MainActivity.java
@@ -3,7 +3,6 @@ package io.github.dogganidhal.fun_prize;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.sglib.demogame.UnityPlayerActivity;
 
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugin.common.MethodChannel;
@@ -21,10 +20,11 @@ public class MainActivity extends FlutterActivity {
     new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
       (call, result) -> {
         if (START_GAME_METHOD.equals(call.method)) {
-          Intent intent = new Intent(MainActivity.this, UnityPlayerActivity.class);
+          Intent intent = new Intent(MainActivity.this, UnityGameActivity.class);
           startActivity(intent);
           result.success(null);
         }
       });
   }
+
 }
---
+++ android/app/src/main/java/io/github/dogganidhal/fun_prize/UnityGameActivity.java
@@ -0,0 +1,7 @@
+package io.github.dogganidhal.fun_prize;
+
+import io.github.dogganidhal.prizefun.android.UnityPlayerActivity;
+
+public class UnityGameActivity extends UnityPlayerActivity {
+
+}
