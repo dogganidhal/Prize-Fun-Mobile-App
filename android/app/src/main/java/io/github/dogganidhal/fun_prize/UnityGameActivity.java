727fb88983984f75b5b5af5a87f46df260fc589b
DOGGA Nidhal <dogga.nidhal@gmail.com>
2019-11-13 23:00:04 +0100

    -- Integrate full unity game

--- android/app/src/main/java/io/github/dogganidhal/fun_prize/UnityGameActivity.java
+++ android/app/src/main/java/io/github/dogganidhal/fun_prize/UnityGameActivity.java
@@ -1,6 +1,6 @@
 package io.github.dogganidhal.fun_prize;
 
-import io.github.dogganidhal.prizefun.android.UnityPlayerActivity;
+import io.github.dogganidhal.fun_prize.unitylib.UnityPlayerActivity;
 
 public class UnityGameActivity extends UnityPlayerActivity {
 
