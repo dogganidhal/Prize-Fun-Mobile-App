3e6707d833d53a137a57fab161a4c66af854e717
Nidhal Dogga <dogga.nidhal@gmail.com>
2020-10-26 12:30:34 +0100

    Get fun points upon register

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:fun_prize/widgets/app.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 
 void main() async {
+  SharedPreferences.setMockInitialValues({});
   WidgetsFlutterBinding.ensureInitialized();
   runApp(App());
 }
